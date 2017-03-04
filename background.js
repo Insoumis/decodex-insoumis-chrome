Insoumis/decodex-insoumis-firefox

reindent background.js + add _debug

background.js
@@ -90,16 +90,17 @@ var sources       = '';
 
 
 function onInstall() {
-    console && console.log("Le Décodex insoumis est installé");
+    if (1 <= _debug)
+        console && console.log("Le Décodex insoumis est installé");
     loadData();
     var last_update = new Date();
     browser.storage.local.set(
-        {
-            'infobulles': [false, true, true, true, false],
-            "installed" : true,
-            'last_update': last_update.getTime(),
-        }
-    );
+            {
+                'infobulles': [false, true, true, true, false],
+                "installed" : true,
+                'last_update': last_update.getTime(),
+            }
+            );
     browser.tabs.create({url: "install.html"});
 }
 
@@ -120,8 +121,10 @@ function loadJSON(path, success, error)
         if (xhr.readyState === XMLHttpRequest.DONE) {
             if (xhr.status === 200) {
                 if (success) {
-                    //console && console.info("raw json");
-                    //console && console.log(xhr.responseText);
+                    if (3 <= _debug) {
+                        console && console.info("raw json");
+                        console && console.log(xhr.responseText);
+                    }
                     success(JSON.parse(xhr.responseText));
                 }
             } else {
@@ -135,31 +138,39 @@ function loadJSON(path, success, error)
 }
 
 function loadData(){
+
+    if (1 <= _debug)
+        console && console.log('start loadData');
     browser.storage.local.get('last_update', function(results){
-console && console.log('updating datas');
         var new_update = new Date();
         loadJSON(base_url,
-             function(data) {
-                browser.storage.local.set({'urls': data['urls']}, function() {
-                });
-                browser.storage.local.set({'sites': data['sites']}, function() {
-                });
-                browser.storage.local.set({'last_update': new_update.getTime()}, function() {
-                });
+                function(data) {
+                    if (1 <= _debug)
+                        console && console.info("set urls to", data['urls']);
+                    browser.storage.local.set({'urls': data['urls']}, function() {
+                    });
+                    if (1 <= _debug)
+                        console && console.info("set sites to", data['sites']);
+                    browser.storage.local.set({'sites': data['sites']}, function() {
+                    });
+                    if (1 <= _debug)
+                        console && console.info("set last_update to", new_update.getTime());
+                    browser.storage.local.set({'last_update': new_update.getTime()}, function() {
+                    });
 
-             }
-        );
+                }
+                );
     });
 }
 
 
 function lastSlash(u){
-	if(u.lastIndexOf('/') == u.length-1) {
+    if(u.lastIndexOf('/') == u.length-1) {
         return u.substring(0, u.length-1);
     }
-	else {
-		return u;	
-	}
+    else {
+        return u;	
+    }
 }
 
 
@@ -179,11 +190,14 @@ function youtubeChannel(u){
 
 
 function debunkSite(u, t, d){
-    console && console.log('debunk site ', u);
-	// TODO: rajouter les champs manquants
+    if (1 <= _debug)
+        console && console.log('debunk site ', u);
+    // TODO: rajouter les champs manquants
     browser.storage.local.get(['urls', "sites", "already_visited", "infobulles", "last_update"], function(results){
-		console && console.info("debunkSite : var results");
-		console && console.log(results);
+        if (1 <= _debug) {
+            console && console.info("debunkSite : var results");
+            console && console.log(results);
+        }
         urls = results.urls;
         sites = results.sites;
         debunker = urls.hasOwnProperty(u);
@@ -200,11 +214,14 @@ function debunkSite(u, t, d){
                 conflits = sites[site_id][7];                  // exemple de conflits / complicité idéologique
                 subventions = sites[site_id][8];               // Montant des subventions d'état
                 sources = sites[site_id][9];                   // Nos sources (urls séparés par virgule et/ou espace)
-                console && console.info("tout s'est bien passé", sites[site_id]);
+                if (1 <= _debug)
+                    console && console.info("tout s'est bien passé", sites[site_id]);
             } catch(e) {
-                console && console.error("ERREUR DEBUNKER");
-                console && console.error(e);
-                console && console.log(sites[site_id]);
+                if (1 <= _debug) {
+                    console && console.error("ERREUR DEBUNKER");
+                    console && console.error(e);
+                    console && console.log(sites[site_id]);
+                }
             }
             browser.browserAction.setIcon({
                 path: "img/icones/icon" + (soumission) + ".png", // note
@@ -218,7 +235,9 @@ function debunkSite(u, t, d){
             }
         }
         else {
-			console && console.log("site non trouvé");
+            if (1 <= _debug) {
+                console && console.log("site non trouvé");
+            }
             browser.browserAction.setIcon({
                 path: "icone.png",
                 tabId: t
@@ -226,11 +245,16 @@ function debunkSite(u, t, d){
         }
         var today = new Date();
         if(always_refresh || (today.getTime() - results.last_update)/1000/60/60 >= 1) {
-			console && console.log("refresh every hour");
+
+            if (1 <= _debug) {
+                console && console.log("refresh every hour");
+            }
             loadData();
         } else {
-			console && console.log("data found in cache");
-		}
+            if (1 <= _debug) {
+                console && console.log("data found in cache");
+            }
+        }
     });
 }
 
@@ -243,51 +267,51 @@ function checkSite(do_display){
         }
         active_url = lastSlash(tab.url);
         if(active_url.indexOf("chrome-extension://") == 0) { return;}
-            // YOUTUBE
-            if(active_url.indexOf("youtube.com/") > -1){
-                if(active_url.indexOf("channel") == -1){
-                    browser.tabs.query({active: true, currentWindow: true}, function(tabs) {
-                        browser.tabs.sendMessage(tabs[0].id, {text: 'report_back'}, function(response){
-                            clean_url = response.farewell.replace('https://www.', "");
-                            debunkSite(clean_url, tab.id, do_display);
-                        });
+        // YOUTUBE
+        if(active_url.indexOf("youtube.com/") > -1){
+            if(active_url.indexOf("channel") == -1){
+                browser.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                    browser.tabs.sendMessage(tabs[0].id, {text: 'report_back'}, function(response){
+                        clean_url = response.farewell.replace('https://www.', "");
+                        debunkSite(clean_url, tab.id, do_display);
                     });
-                }
-                else {
-                    clean_url = youtubeChannel(url_cleaner(active_url));
-                    debunkSite(clean_url, tab.id, do_display);
-                }
+                });
             }
-            // SOCIAL NETWORKS HOMEPAGE
-            else if(active_url == 'https://www.facebook.com' || active_url == 'https://twitter.com' || active_url == 'https://www.youtube.com'){
-                clean_url = url_cleaner(active_url);
+            else {
+                clean_url = youtubeChannel(url_cleaner(active_url));
                 debunkSite(clean_url, tab.id, do_display);
             }
-            // OTHER URLS
-            else {
-                matches = []
+        }
+        // SOCIAL NETWORKS HOMEPAGE
+        else if(active_url == 'https://www.facebook.com' || active_url == 'https://twitter.com' || active_url == 'https://www.youtube.com'){
+            clean_url = url_cleaner(active_url);
+        debunkSite(clean_url, tab.id, do_display);
+    }
+    // OTHER URLS
+        else {
+            matches = []
                 for (var key in urls) {
                     if (!urls.hasOwnProperty(key)) continue;
                     var index = active_url.indexOf(key);
                     if(index != -1) {
                         if((active_url.indexOf('http://www.'+ key) == 0 || active_url.indexOf('https://www.'+ key) == 0 || active_url.indexOf('http://'+ key) == 0 || active_url.indexOf('https://'+ key) == 0)
-                            && index != 0
-                            && (active_url[index-1] == "/" || active_url[index-1] == ".")
-                            && key != "facebook.com"
-                            && key != "twitter.com") {
-                                matches.push(key);
+                                && index != 0
+                                && (active_url[index-1] == "/" || active_url[index-1] == ".")
+                                && key != "facebook.com"
+                                && key != "twitter.com") {
+                            matches.push(key);
                         }
                     }
                 }
-                tampon = "";
-                for(var url_i=0;url_i<matches.length;url_i++){
-                    if(matches[url_i].length > tampon.length){
-                        tampon = matches[url_i];
-                    }
+            tampon = "";
+            for(var url_i=0;url_i<matches.length;url_i++){
+                if(matches[url_i].length > tampon.length){
+                    tampon = matches[url_i];
                 }
-                clean_url = tampon;
-                debunkSite(clean_url, tab.id, do_display);
             }
+            clean_url = tampon;
+            debunkSite(clean_url, tab.id, do_display);
+        }
     });
 }
 

Insoumis/decodex-insoumis-chrome

background.js
@@ -113,8 +113,10 @@ function loadJSON(path, success, error)
         if (xhr.readyState === XMLHttpRequest.DONE) {
             if (xhr.status === 200) {
                 if (success) {
-                    //console && console.info("raw json");
-                    //console && console.log(xhr.responseText);
+                    if (3 <= _debug) {
+                        console && console.info("raw json");
+                        console && console.log(xhr.responseText);
+                    }
                     success(JSON.parse(xhr.responseText));
                 }
             } else {
@@ -129,31 +131,39 @@ function loadJSON(path, success, error)
 
 
 function loadData(){
+
+    if (1 <= _debug)
+        console && console.log('start loadData');
     chrome.storage.local.get('last_update', function(results){
-console && console.log('updating datas');
         var new_update = new Date();
         loadJSON(base_url,
-             function(data) {
-                chrome.storage.local.set({'urls': data['urls']}, function() {
-                });
-                chrome.storage.local.set({'sites': data['sites']}, function() {
-                });
-                chrome.storage.local.set({'last_update': new_update.getTime()}, function() {
-                });
+                function(data) {
+                    if (1 <= _debug)
+                        console && console.info("set urls to", data['urls']);
+                    chrome.storage.local.set({'urls': data['urls']}, function() {
+                    });
+                    if (1 <= _debug)
+                        console && console.info("set sites to", data['sites']);
+                    chrome.storage.local.set({'sites': data['sites']}, function() {
+                    });
+                    if (1 <= _debug)
+                        console && console.info("set last_update to", new_update.getTime());
+                    chrome.storage.local.set({'last_update': new_update.getTime()}, function() {
+                    });
 
-             }
-        );
+                }
+                );
     });
 }
 
 
 function lastSlash(u){
-	if(u.lastIndexOf('/') == u.length-1) {
+    if(u.lastIndexOf('/') == u.length-1) {
         return u.substring(0, u.length-1);
     }
-	else {
-		return u;	
-	}
+    else {
+        return u;	
+    }
 }
 
 
@@ -173,11 +183,14 @@ function youtubeChannel(u){
 
 
 function debunkSite(u, t, d){
-    console && console.log('debunk site ', u);
-	// TODO: rajouter les champs manquants
+    if (1 <= _debug)
+        console && console.log('debunk site ', u);
+    // TODO: rajouter les champs manquants
     chrome.storage.local.get(['urls', "sites", "already_visited", "infobulles", "last_update"], function(results){
-		console && console.info("debunkSite : var results");
-		console && console.log(results);
+        if (1 <= _debug) {
+            console && console.info("debunkSite : var results");
+            console && console.log(results);
+        }
         urls = results.urls;
         sites = results.sites;
         debunker = urls.hasOwnProperty(u);
@@ -194,11 +207,14 @@ function debunkSite(u, t, d){
                 conflits = sites[site_id][7];                  // exemple de conflits / complicité idéologique
                 subventions = sites[site_id][8];               // Montant des subventions d'état
                 sources = sites[site_id][9];                   // Nos sources (urls séparés par virgule et/ou espace)
-                console && console.info("tout s'est bien passé", sites[site_id]);
+                if (1 <= _debug)
+                    console && console.info("tout s'est bien passé", sites[site_id]);
             } catch(e) {
-                console && console.error("ERREUR DEBUNKER");
-                console && console.error(e);
-                console && console.log(sites[site_id]);
+                if (1 <= _debug) {
+                    console && console.error("ERREUR DEBUNKER");
+                    console && console.error(e);
+                    console && console.log(sites[site_id]);
+                }
             }
 
             chrome.browserAction.setIcon({
@@ -213,7 +229,9 @@ function debunkSite(u, t, d){
             }
         }
         else {
-			console && console.log("site non trouvé");
+            if (1 <= _debug) {
+                console && console.log("site non trouvé");
+            }
             chrome.browserAction.setIcon({
                 path: "icone.png",
                 tabId: t
@@ -221,11 +239,16 @@ function debunkSite(u, t, d){
         }
         var today = new Date();
         if(always_refresh || (today.getTime() - results.last_update)/1000/60/60 >= 1) {
-			console && console.log("refresh every hour");
+
+            if (1 <= _debug) {
+                console && console.log("refresh every hour");
+            }
             loadData();
         } else {
-			console && console.log("data found in cache");
-		}
+            if (1 <= _debug) {
+                console && console.log("data found in cache");
+            }
+        }
     });
 }
 
@@ -234,52 +257,52 @@ function checkSite(do_display){
     chrome.tabs.getSelected(null,function(tab) {
         active_url = lastSlash(tab.url);
         if(active_url.indexOf("chrome-extension://") == 0) { return;}
-            // YOUTUBE
-            if(active_url.indexOf("youtube.com/") > -1){
-                if(active_url.indexOf("channel") == -1){
-                    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-                        chrome.tabs.sendMessage(tabs[0].id, {text: 'report_back'}, function(response){
-                            clean_url = response.farewell.replace('https://www.', "");
-                            debunkSite(clean_url, tab.id, do_display);
-                        });
+        // YOUTUBE
+        if(active_url.indexOf("youtube.com/") > -1){
+            if(active_url.indexOf("channel") == -1){
+                chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                    chrome.tabs.sendMessage(tabs[0].id, {text: 'report_back'}, function(response){
+                        clean_url = response.farewell.replace('https://www.', "");
+                        debunkSite(clean_url, tab.id, do_display);
                     });
-                }
-                else {
-                    clean_url = youtubeChannel(url_cleaner(active_url));
-                    debunkSite(clean_url, tab.id, do_display);
-                }
+                });
             }
-            // SOCIAL NETWORKS HOMEPAGE
-            else if(active_url == 'https://www.facebook.com' || active_url == 'https://twitter.com' || active_url == 'https://www.youtube.com'){
-                clean_url = url_cleaner(active_url);
+            else {
+                clean_url = youtubeChannel(url_cleaner(active_url));
                 debunkSite(clean_url, tab.id, do_display);
             }
-            // OTHER URLS
-            else {
-                matches = []
+        }
+        // SOCIAL NETWORKS HOMEPAGE
+        else if(active_url == 'https://www.facebook.com' || active_url == 'https://twitter.com' || active_url == 'https://www.youtube.com'){
+            clean_url = url_cleaner(active_url);
+        debunkSite(clean_url, tab.id, do_display);
+    }
+    // OTHER URLS
+        else {
+            matches = []
                 for (var key in urls) {
                     if (!urls.hasOwnProperty(key)) continue;
                     var index = active_url.indexOf(key);
                     if(index != -1) {
                     console.log(active_url + " --- " + key);
                         if((active_url.indexOf('http://www.'+ key) == 0 || active_url.indexOf('https://www.'+ key) == 0 || active_url.indexOf('http://'+ key) == 0 || active_url.indexOf('https://'+ key) == 0)
-                            && index != 0
-                            && (active_url[index-1] == "/" || active_url[index-1] == ".")
-                            && key != "facebook.com"
-                            && key != "twitter.com") {
-                                matches.push(key);
+                                && index != 0
+                                && (active_url[index-1] == "/" || active_url[index-1] == ".")
+                                && key != "facebook.com"
+                                && key != "twitter.com") {
+                            matches.push(key);
                         }
                     }
                 }
-                tampon = "";
-                for(var url_i=0;url_i<matches.length;url_i++){
-                    if(matches[url_i].length > tampon.length){
-                        tampon = matches[url_i];
-                    }
+            tampon = "";
+            for(var url_i=0;url_i<matches.length;url_i++){
+                if(matches[url_i].length > tampon.length){
+                    tampon = matches[url_i];
                 }
-                clean_url = tampon;
-                debunkSite(clean_url, tab.id, do_display);
             }
+            clean_url = tampon;
+            debunkSite(clean_url, tab.id, do_display);
+        }
     });
 }
 