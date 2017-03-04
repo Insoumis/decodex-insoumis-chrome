c7cf0e44dad17cf05c3f2d7b85551262c26ae2e0
Michaël Marinetti <github@marinetti.fr>
2017-03-04 18:42:09 +0100

    Mod: small changes for refresh

--- background.js
+++ background.js
@@ -130,7 +130,7 @@ function loadJSON(path, success, error)
         if (xhr.readyState === XMLHttpRequest.DONE) {
             if (xhr.status === 200) {
                 if (success) {
-                    if (3 <= _debug) {
+                    if (4 <= _debug) {
                         console && console.info("raw json");
                         console && console.log(xhr.responseText);
                     }
@@ -150,24 +150,31 @@ function loadJSON(path, success, error)
 function loadData(){
 
     if (1 <= _debug)
-r       console && console.log('start loadData');
+       console && console.log('start loadData');
     chrome.storage.local.get('last_update', function(results){
         var new_update = new Date();
+        if (2 <= _debug) {
+            console && console.log("found last update : ", results, "base url=", base_url+"?"+new_update.getTime());
+        }
         loadJSON(base_url+"?"+new_update.getTime(),
                 function(data) {
-                    if (1 <= _debug)
-                        console && console.info("set urls to", data['urls']);
+                    if (2 <= _debug) {
+                        console && console.info("storing urls...", data['urls']);
+                    }
                     chrome.storage.local.set({'urls': data['urls']}, function() {
                     });
-                    if (1 <= _debug)
+                    if (2 <= _debug)
                         console && console.info("set sites to", data['sites']);
                     chrome.storage.local.set({'sites': data['sites']}, function() {
                     });
-                    if (1 <= _debug)
+                    if (3 <= _debug)
                         console && console.info("set last_update to", new_update.getTime());
                     chrome.storage.local.set({'last_update': new_update.getTime()}, function() {
                     });
 
+                },
+                function(data) {
+                    console && console.error("error on loadJSON", data);
                 }
                 );
     });
@@ -200,7 +207,7 @@ function youtubeChannel(u){
 
 
 function debunkSite(u, t, d){
-    if (2 <= _debug)
+    if (3 <= _debug)
         console && console.log('debunk site ', u);
 
     chrome.storage.local.get(['urls', "sites", "already_visited", "infobulles", "last_update"], function(results){
@@ -208,110 +215,113 @@ function debunkSite(u, t, d){
             console && console.info("debunkSite : var results");
             console && console.log(results);
         }
-        urls = results.urls;
-        sites = results.sites;
-        debunker = urls.hasOwnProperty(u);
-        if (debunker == true) {
-            site_id = urls[u];
-            if (2 <= _debug) {
-                console && console.log('site FOUND ! ', site_id);
-            }
-            try {
-                site_actif     = sites[site_id][col_nom];                    // nom du site
-                note_decodex   = parseInt(sites[site_id][col_note_decodex]); // note decodex
-                soumission     = parseInt(sites[site_id][col_insoumis]);     // note insoumis
-                notule         = sites[site_id][col_desc];                   // description originale
-                slug           = sites[site_id][col_slug];                   // nom normalisé
-                proprietaires  = sites[site_id][col_proprietaire];           // propriétaires
-                interets       = sites[site_id][col_interet];                // intérets
-                conflits       = sites[site_id][col_exemple];                // exemple de conflits / complicité idéologique
-                subventions    = sites[site_id][col_subventions];            // Montant des subventions d'état
-                publicite      = sites[site_id][col_pub];                    // Pub ?
-                sources        = sites[site_id][col_sources];                // Nos sources (urls séparés par virgule et/ou espace)
-
-                if (3 <= _debug) {
-                    console && console.info("sources avant markdown", sources);
-                }
-                // Markdown style
-                sources = sources.replace(/\[([^\]]*?)\]\(([^\)]*?)\)[, ]{0,2}/gm, '<a class="source-link" href="$2">$1</a>');
-                if (3 <= _debug) {
-                    console && console.log("sources apres markdown", sources);
-                }
-                // URL toute seule (a corriger)
-                sources = sources.replace(/^(http[s]?:\/\/([^/]+)\/[^" ,]+)[^"]{1,2}$/g, '<a href="$1">$2</a><br>');
-                if (3 <= _debug) {
-                    console && console.log("sources apres urls simples", sources);
-                }
-
+        try {
+            urls = results.urls;
+            sites = results.sites;
+            debunker = urls.hasOwnProperty(u);
+            if (debunker == true) {
+                site_id = urls[u];
                 if (2 <= _debug) {
-                    console && console.group("tout s'est bien passé");
-                    console && console.log('site_actif     =',site_actif     );
-                    console && console.log('note_decodex   =',note_decodex   );
-                    console && console.log('soumission     =',soumission     );
-                    console && console.log('notule         =',notule         );
-                    console && console.log('slug           =',slug           );
-                    console && console.log('proprietaires  =',proprietaires  );
-                    console && console.log('interets       =',interets       );
-                    console && console.log('conflits       =',conflits       );
-                    console && console.log('subventions    =',subventions    );
-                    console && console.log('sources        =',sources        );
-                    console && console.groupEnd();
-
+                    console && console.log('site FOUND ! ', site_id);
                 }
-            } catch(e) {
-                if (1 <= _debug) {
-                    console && console.error("ERREUR DEBUNKER");
-                    console && console.error(e);
-                    console && console.log(sites[site_id]);
+                try {
+                    site_actif     = sites[site_id][col_nom];                    // nom du site
+                    note_decodex   = parseInt(sites[site_id][col_note_decodex]); // note decodex
+                    soumission     = parseInt(sites[site_id][col_insoumis]);     // note insoumis
+                    notule         = sites[site_id][col_desc];                   // description originale
+                    slug           = sites[site_id][col_slug];                   // nom normalisé
+                    proprietaires  = sites[site_id][col_proprietaire];           // propriétaires
+                    interets       = sites[site_id][col_interet];                // intérets
+                    conflits       = sites[site_id][col_exemple];                // exemple de conflits / complicité idéologique
+                    subventions    = sites[site_id][col_subventions];            // Montant des subventions d'état
+                    publicite      = sites[site_id][col_pub];                    // Pub ?
+                    sources        = sites[site_id][col_sources];                // Nos sources (urls séparés par virgule et/ou espace)
+
+                    if (3 <= _debug) {
+                        console && console.info("sources avant markdown", sources);
+                    }
+                    // Markdown style
+                    sources = sources.replace(/\[([^\]]*?)\]\(([^\)]*?)\)[, ]{0,2}/gm, '<a class="source-link" href="$2">$1</a>');
+                    if (3 <= _debug) {
+                        console && console.log("sources apres markdown", sources);
+                    }
+                    // URL toute seule (a corriger)
+                    sources = sources.replace(/^(http[s]?:\/\/([^/]+)\/[^" ,]+)[^"]{1,2}$/g, '<a href="$1">$2</a><br>');
+                            if (3 <= _debug) {
+                                console && console.log("sources apres urls simples", sources);
+                            }
+
+                            if (2 <= _debug) {
+                                console && console.group("tout s'est bien passé");
+                                console && console.log('site_actif     =',site_actif     );
+                                console && console.log('note_decodex   =',note_decodex   );
+                                console && console.log('soumission     =',soumission     );
+                                console && console.log('notule         =',notule         );
+                                console && console.log('slug           =',slug           );
+                                console && console.log('proprietaires  =',proprietaires  );
+                                console && console.log('interets       =',interets       );
+                                console && console.log('conflits       =',conflits       );
+                                console && console.log('subventions    =',subventions    );
+                                console && console.log('sources        =',sources        );
+                                console && console.groupEnd();
+
+                            }
+                            } catch(e) {
+                                if (1 <= _debug) {
+                                    console && console.error("ERREUR DEBUNKER");
+                                    console && console.error(e);
+                                    console && console.log(sites[site_id]);
+                                }
+                            }
+                chrome.browserAction.setIcon({
+                    path: "img/icones/icon" + (soumission) + ".png", // note
+                    tabId: t
+                });
+                if(results.infobulles[soumission] == true && d == true){  // note
+                    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                        chrome.tabs.sendMessage(tabs[0].id, {text: "soumission"+soumission}, function(response) { // note
+                        });
+                    });
                 }
             }
-
-            chrome.browserAction.setIcon({
-                path: "img/icones/icon" + (soumission) + ".png", // note
-                tabId: t
-            });
-            if(results.infobulles[soumission] == true && d == true){  // note
-                chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-                    chrome.tabs.sendMessage(tabs[0].id, {text: "soumission"+soumission}, function(response) { // note
-                    });
+            else {
+                if (2 <= _debug) {
+                    console && console.info("site non trouvé", u);
+                    console && console.log(u);
+                }
+                chrome.browserAction.setIcon({
+                    path: "icone.png",
+                    tabId: t
                 });
             }
-        }
-        else {
-            if (2 <= _debug) {
-                console && console.info("site non trouvé", u);
-                console && console.log(u);
+
+            if (u.match(/youtube.com/)) {
+
+                if (null == soumission)
+                    soumission  = 0;                             // propriétaires
+
+                chrome.browserAction.setIcon({
+                    path: "img/icones/icon" + (soumission) + ".png", // note
+                    tabId: t
+                });
+
+                if ("" == proprietaires)
+                    proprietaires  = "Youtube est une propriété de la Holding Alphabet (Google)";                             // propriétaires
+                if ("" == interets)
+                    interets       = "Le groupe Alphabet(Google) a de nombreux intérêts internationnaux. Son business model est fortement basé sur la publicité et son quasi-monopole de la publicité. Google exerce de nombreuses pressions sur les états et l'Union Européenne.";                               // intérets
+                if ("" == conflits)
+                    conflits       = "Youtube peut être un outil de partage de connaissances. Les vidéastes et utilisateurs de la plateforme youtube ne sont pas forcément soumis à Google, mais… ";  // exemple de conflits / complicité idéologique
+                if ("" == subventions)
+                    subventions    = "";             // Montant des subventions d'état
+                if ("" == sources)
+                    sources        = "";             // Nos sources (urls séparés par virgule et/ou espace)
             }
-            chrome.browserAction.setIcon({
-                path: "icone.png",
-                tabId: t
-            });
-        }
-
-        if (u.match(/youtube.com/)) {
-
-            if (null == soumission)
-                soumission  = 0;                             // propriétaires
-
-            chrome.browserAction.setIcon({
-                path: "img/icones/icon" + (soumission) + ".png", // note
-                tabId: t
-            });
-
-            if ("" == proprietaires)
-                proprietaires  = "Youtube est une propriété de la Holding Alphabet (Google)";                             // propriétaires
-            if ("" == interets)
-                interets       = "Le groupe Alphabet(Google) a de nombreux intérêts internationnaux. Son business model est fortement basé sur la publicité et son quasi-monopole de la publicité. Google exerce de nombreuses pressions sur les états et l'Union Européenne.";                               // intérets
-            if ("" == conflits)
-                conflits       = "Youtube peut être un outil de partage de connaissances. Les vidéastes et utilisateurs de la plateforme youtube ne sont pas forcément soumis à Google, mais… ";  // exemple de conflits / complicité idéologique
-            if ("" == subventions)
-                subventions    = "";             // Montant des subventions d'état
-            if ("" == sources)
-                sources        = "";             // Nos sources (urls séparés par virgule et/ou espace)
+        } catch(e) {
+            console && console.error(e);
         }
 
         var today = new Date();
-        if(always_refresh || (today.getTime() - results.last_update)/1000/60/60 >= 1) {
+        if(always_refresh || (today.getTime() - results.last_update)/1000/60/60 >= 24) {
 
             if (1 <= _debug) {
                 console && console.log("refresh every hour or refresh forced");
@@ -329,11 +339,13 @@ function debunkSite(u, t, d){
 function checkSite(do_display){
     chrome.tabs.getSelected(null,function(tab) {
         active_url = lastSlash(tab.url);
-        if (_debug > 1) {
+        if (_debug > 5) {
             console && console.warn("active url", active_url);
         }
 
-        if(active_url.indexOf("chrome-extension://") == 0) { return;}
+        if(active_url.indexOf("chrome-extension://") == 0) {
+            return;
+        }
         // YOUTUBE
         if(active_url.indexOf("youtube.com/") > -1){
             if(active_url.indexOf("channel") == -1){
