65fbf051677afea534d1c23773cf47e2161ba22e
Michaël Marinetti <github@marinetti.fr>
2017-03-07 12:21:06 +0100

    Add: basic syntax check before deploy

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,55 @@
 #!/bin/bash
 
-tag=$(git describe --exact-match --tags HEAD|sed "s/chrome-//")
+# Validation (require json_pp)
+#cat manifest.json|json_pp
+#if [[ $? ]]; then
+#    echo "syntax error manifest.json"
+#    exit 128
+#fi
+
+node -c background.js
+if [[ $? ]]; then
+    echo "syntax error background.js"
+    exit 128
+fi
+
+node -c install.js
+if [[ $? ]]; then
+    echo "syntax error install.js"
+    exit 128
+fi
+
+node -c popup.js
+if [[ $? ]]; then
+    echo "syntax error popup.js"
+    exit 128
+fi
+
+node -c content.js
+if [[ $? ]]; then
+    echo "syntax error content.js"
+    exit 128
+fi
+
+
+tag=$(git describe --exact-match --tags HEAD)
+
 no_tag=$?
 if [[ $no_tag -ne 0 ]]; then
-    echo "no tag found in current HEAD. aborting"
-    exit 1;
+    echo "No tag found in current HEAD. Do not deploy."
+    exit 0;
+fi
+
+tag=$(echo $tag|sed "s/chrome-//")
+if [[ -z "$tag" ]]; then
+    echo "invalid tag (it should contains «chrome-»). Do not deploy."
+    exit 0;
 fi
 
 version=$(cat manifest.json|grep $tag|awk '{print $2}');
 if [[ -z "$version" ]]; then
-    echo "version and tag ($tag) does not match";
-    exit 2;
+    echo "version and tag ($tag) does not match. Do not deploy";
+    exit 0;
 fi
 echo "version in manifest matches : $version"
 
